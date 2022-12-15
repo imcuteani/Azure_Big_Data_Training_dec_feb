ebf2e0f0800645b882068bdd07b0cceccf4e5b90
Anindita Basak <anindita.basak@live.com>
2022-12-15 11:10:05 +0530

    added DML queries for MSSQL

--- Hands-on/SQL_commands/DML/insert_data.sql
+++ Hands-on/SQL_commands/DML/insert_data.sql
@@ -1,6 +1,15 @@
 use "cgsqltraining"
 go 
 
+
+SET IDENTITY_INSERT [Customers].[Customer] ON
+INSERT INTO Customers.Customer(CustomerID, CompanyName, FirstName, LastName, ModifiedDate) VALUES (1, 'AdventureWorks', 'James', 'Webb', '2022-01-01')
+INSERT INTO Customers.Customer(CustomerID, CompanyName, FirstName, LastName, ModifiedDate) VALUES (2, 'Fabrikum', 'Matt', 'Hardin', '2021-01-01')
+GO
+
+-- Set IDENTITY_INSERT OFF 
+SET IDENTITY_INSERT [Customers].[Customer] OFF
+
 SET IDENTITY_INSERT [Customers].[CustomerAddress] ON
 INSERT INTO Customers.CustomerAddress
 (AddressId,
@@ -21,3 +30,19 @@ VALUES
  '',
  'united states')
 GO
+
+use "cgsqltraining"
+go 
+
+SET IDENTITY_INSERT [Products].[Product] ON 
+INSERT INTO Products.Product (ProductID, ProductName, ProductCost, ListPrice, ProductDescription) VALUES (1, 'Samsung', 5.66, 4.55,'Refrigerator')
+INSERT INTO Products.Product (ProductID, ProductName, ProductCost, ListPrice, ProductDescription) VALUES (2, 'Sansui', 7.66, 16.55,'Washing Machine')
+INSERT INTO Products.Product (ProductID, ProductName, ProductCost, ListPrice, ProductDescription) VALUES (3, 'Samsung', 20.66, 27.55,'Laptop')
+INSERT INTO Products.Product (ProductID, ProductName, ProductCost, ListPrice, ProductDescription) VALUES (4, 'HP', 9.66, 15.55,'Smart TV')
+INSERT INTO Products.Product (ProductID, ProductName, ProductCost, ListPrice, ProductDescription) VALUES (5, 'Lloyds', 10.66, 24.55,'AC')
+INSERT INTO Products.Product (ProductID, ProductName, ProductCost, ListPrice, ProductDescription) VALUES (6, 'Samsung', 17.66, 34.55,'Refrigerator')
+INSERT INTO Products.Product (ProductID, ProductName, ProductCost, ListPrice, ProductDescription) VALUES (7, 'Samsung', 8.66, 23.55,'Refrigerator')
+INSERT INTO Products.Product (ProductID, ProductName, ProductCost, ListPrice, ProductDescription) VALUES (8, 'Samsung', 4.66, 45.55,'Printer')
+INSERT INTO Products.Product (ProductID, ProductName, ProductCost, ListPrice, ProductDescription) VALUES (9, 'Daikin', 6.66, 12.55,'Refrigerator')
+
+select * from Products.Product
