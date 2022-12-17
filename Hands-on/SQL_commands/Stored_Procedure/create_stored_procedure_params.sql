a2adb11973b6dbbe52259fb314bd45e46e46d419
Anindita Basak <anindita.basak@live.com>
2022-12-17 16:20:46 +0530

    added Stored proc multiple params

--- Hands-on/SQL_commands/Stored_Procedure/create_stored_procedure_params.sql
+++ Hands-on/SQL_commands/Stored_Procedure/create_stored_procedure_params.sql
@@ -10,5 +10,30 @@ AS
 SELECT * 
 FROM Person.Address
 WHERE City LIKE @City + '%'
+GO
+
+-- Invoke the Stored procedure with Wildcard parameter
+
+EXEC dbo.uspGetAddressWildCard @City = 'New' 
+GO
+
+-- Stored procedure with multiple parameters 
+
+CREATE PROCEDURE dbo.uspGetMultiParam @City NVARCHAR(50) = NULL,
+ @AddressLine1 NVARCHAR(60) = NULL
+AS 
+SELECT * 
+FROM Person.Address 
+WHERE City = ISNULL(@City, City)
+AND AddressLine1 LIKE '%' + ISNULL(@AddressLine1, AddressLine1) + '%'
+GO
+
+-- execute/ call the stored procedure 
+
+EXEC dbo.uspGetMultiParam @City = 'Calgary', @AddressLine1 = 'A'
 GO 
+
+EXEC dbo.uspGetMultiParam @City = 'Calgary', @AddressLine1 = 'S'
+GO 
+
 
