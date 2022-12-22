6239b92467e83971526098881d62b5b8d0325290
Anindita Basak <anindita.basak@live.com>
2022-12-22 17:22:59 +0530

    added PS providers

---
+++ Hands-on/PowerShell/Providers/providers.ps1
@@ -0,0 +1,14 @@
+# PowerShell Provider 
+
+# A provider allows to access data and components which would not otherwise be easily accessible at the command line. 
+# the data is presented in a consistent format which resembles a file system drive. 
+
+# The data could be exposed on drive, and it can accessed via a path like a disk drive 
+
+# Get information about one or more providers 
+
+# Get-PSProvider
+
+# Get provider information 
+
+Get-PSDrive Function | Format-List * 
