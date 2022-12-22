9e1bf203f09c25154edf5296b0a15c3687d2d5f8
Anindita Basak <anindita.basak@live.com>
2022-12-22 11:47:37 +0530

    added operators in PS

---
+++ Hands-on/PowerShell/cmdlets/Operators.ps1
@@ -0,0 +1,39 @@
+# Operators in PowerShell 
+
+# In PowerShell scripting, there're following operators are available 
+# 1. Arithemetic operator 
+# 2. Assignment operator 
+# 3. Comparison operator
+# 4. Logical operator
+# 5. Redirectional operator
+# 6. Split and join operator
+# 7. Type operator 
+# 8. Unary operator 
+
+# Arithemetic operators in PS 
+
+$a = 10 
+$b = 20
+$c = 25
+$d = 30 
+
+# addition 
+
+$a + $b 
+
+# substraction 
+$a - $b 
+
+# multiplication 
+
+$a * $b 
+
+# division 
+
+$b / $a 
+
+# Modulus 
+
+$b % $a 
+
+
