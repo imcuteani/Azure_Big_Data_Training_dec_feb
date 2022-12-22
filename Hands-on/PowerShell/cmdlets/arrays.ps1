bf9e5d8d7530010390c473f301caf038eaaf32f0
Anindita Basak <anindita.basak@live.com>
2022-12-22 15:34:05 +0530

    added array additional scripts

--- Hands-on/PowerShell/cmdlets/arrays.ps1
+++ Hands-on/PowerShell/cmdlets/arrays.ps1
@@ -25,3 +25,35 @@ $mylist
 
 $mylist[8] = 10.44
 $mylist
+
+# removing an element from a specific index position of an array
+
+[System.Collections.ArrayList]$myList = "item1", "item2", "item3", "item4", "item5"
+$myList.RemoveAt(2)
+$myList
+
+# removing an element from a specific index position of an array 
+[System.Collections.ArrayList]$mylist = 5.6, 3.4, 6.5, 7.8, 34.0, 23.0, 44.5, 99.45, 110.12
+$mylist.RemoveAt(6)
+$mylist
+
+# Remove() removes the first occurance of a specific object from the System.Collections.ArrayList
+# RemoveAt() removes element at the specific index of the System.Collections.ArrayList 
+
+#$mylist.Add()
+
+#$mylist.Remove("item1")
+#$mylist.IsFixedSize
+
+# Remove the objects from Array list by index position 
+#$index = $myList.IndexOf("item2")
+#$myList.RemoveAt($index)
+
+# Length property to get the count of the length of the array
+
+$array = "item1", "item2", "item3"
+$array.GetType()
+
+# Length property
+$array.Length
+$array.Count
