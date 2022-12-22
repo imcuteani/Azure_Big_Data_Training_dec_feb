9443653a25cb66cc2afb86a8634e3de49545984d
Anindita Basak <anindita.basak@live.com>
2022-12-22 11:04:06 +0530

    added PS cmdlets

---
+++ Hands-on/PowerShell/cmdlets/Get-command.ps1
@@ -0,0 +1,10 @@
+# Get the details of PowerShell alias, Functions, modules 
+Get-Command 
+
+# Display help information about the command Formatted table
+Get-Help Format-Table
+
+
+# Get All services running in your local Windows env 
+
+Get-Service "sql*"
