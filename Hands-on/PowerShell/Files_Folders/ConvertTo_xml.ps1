a83ee3bcb5b5957da51207f238f56f7aa8af8c8e
Anindita Basak <anindita.basak@live.com>
2022-12-22 17:08:45 +0530

    added ConvertTo-xml

---
+++ Hands-on/PowerShell/Files_Folders/ConvertTo_xml.ps1
@@ -0,0 +1,7 @@
+# ConvertTo-XML 
+
+# creates an XML based representation of one of more .NET objects 
+
+# Get the current date and convert the output to XML file 
+
+Get-Date | ConvertTo-Xml | Out-File date.xml
