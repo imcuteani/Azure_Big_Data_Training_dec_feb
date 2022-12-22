ff449f2ed4bbd56ed17e8877e3601ec14aa39ce7
Anindita Basak <anindita.basak@live.com>
2022-12-22 11:09:50 +0530

    added Get-member

--- Hands-on/PowerShell/cmdlets/Get-command.ps1
+++ Hands-on/PowerShell/cmdlets/Get-command.ps1
@@ -8,3 +8,9 @@ Get-Help Format-Table
 # Get All services running in your local Windows env 
 
 Get-Service "sql*"
+
+# Get-Members of the SQL processes running in the system
+# "|" is called pipe operator in PowerShell scripting which's used to concatenate two commands
+
+
+Get-Service "sql*" | Get-Member
