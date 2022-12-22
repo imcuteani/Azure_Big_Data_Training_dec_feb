dd2822841fe61c6dd8f7c720983679881ff0413d
Anindita Basak <anindita.basak@live.com>
2022-12-22 17:01:02 +0530

    added ConvertTo-Html

---
+++ Hands-on/PowerShell/Files_Folders/ConvertTo_html.ps1
@@ -0,0 +1,13 @@
+# ConvertTo-HTML 
+
+# The ConvertTo-HTML cmdlet converts a .NET object into HTML which can be displayed in a web browser
+
+# you can use this cmdlet to display the output of a command in a web page
+
+# you can use the parameters of ConvertTo-Html to select object properties. 
+# to specify a table or list format. to specify this Html page title, to add text 
+
+# create a web page in html to get current date in PS 
+
+ConvertTo-Html -InputObject (Get-Date) | Out-File get-date.html
+Invoke-Item get-date.html
