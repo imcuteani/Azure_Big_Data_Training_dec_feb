d4a72627023adfbe80faf7eba6fc8c0f65fec08a
Anindita Basak <anindita.basak@live.com>
2023-01-25 11:32:34 +0530

    added list and tuples

--- Hands-on/Python/Basics/datatypes_lists_tuples.py
+++ Hands-on/Python/Basics/datatypes_lists_tuples.py
@@ -38,6 +38,45 @@ print(nums[:-2])
 
 nums[2:4] = [8,9]
 print(nums)
+
+# List comprehension values 
+
+# square of a number through python list 
+
+nums = [0, 1, 2, 3, 4]
+squares = []
+for x in nums:
+    squares.append(x ** 2)
+    print(squares)
+
+
+# even of square of numbers, to check whether the number is an even number or not     
+
+nums = [0, 1, 2, 3, 4]
+even_squares = [x ** 2 for x in nums if x % 2 == 0]
+print(even_squares)
+
+
+# Python tuples 
+
+# Tuples are unordered list of elements similar to list 
+
+
+
+# Create a dictionary with tuple keys 
+
+d = {(x, x + 1): x for x in range(10)}
+#t = (5,7)
+#print(type(t))
+#print(d[t])           # print 5 
+print(d[(1, 2)])      # print 1 (tuple key of first element of second index)
+
+tupl = (1, "python", "programming", "spark", 4+5)
+print(tupl)
+print(tupl[3])
+
+
+
  
 
 
