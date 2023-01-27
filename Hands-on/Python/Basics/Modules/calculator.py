567b823193679d687cb47a43b7a768dc5404f178
Anindita Basak <anindita.basak@live.com>
2023-01-27 10:32:04 +0530

    added py modules

---
+++ Hands-on/Python/Basics/Modules/calculator.py
@@ -0,0 +1,20 @@
+# A simple calculator Module in python 
+
+def add(x, y):
+    return (x + y)
+
+def subs(x, y):
+    return (x - y)
+
+def mul(x, y):
+    return x*y
+
+def div(x, y):
+    return x/y
+
+
+
+
+
+    
+
---
+++ Hands-on/Python/Basics/Modules/main.py
@@ -0,0 +1,36 @@
+# Python Module 
+
+# A python module is a file containing a set of codes or a function which can be included to an application. 
+# A module could be a file containing a single variable , a function or a big code base. 
+
+# Simplication 
+# Flexibility - Modules are frequently used to establish conceptual seperations between various problem areas. 
+# it's less likely that changes in one module would influence other parts of the program if modules are constructed 
+# in a fashion that reduces the interconnectedness. 
+
+# Modules are used for reusability purposes. 
+#scope - using of functions, modules and packages. 
+# A document with definitions of functions and various statements are called as python modules. 
+# A module can define functions, classes and variables. 
+# A module can also include runnable code, grouping of related code in module makes the code easier to understand and use. 
+# it also makes the code to be logically organized. 
+
+import calculator
+
+print("Calculator in Python using Modules: ")
+# addition operation
+print(calculator.add(10, 4))
+
+# substraction operation 
+
+print(calculator.subs(15, 5))
+
+# multiplication operation 
+
+print(calculator.mul(10, 8))
+
+# division operation 
+
+print(calculator.div(25, 5))
+
+
