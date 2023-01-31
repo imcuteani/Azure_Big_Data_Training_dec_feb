56d26365dc32264e7f458de5251bae8726b82da0
Anindita Basak <anindita.basak@live.com>
2023-01-31 17:35:20 +0530

    added empty dataframe

--- Hands-on/Apache Spark/Pyspark/pyspark_create_emptyRDD().py
+++ Hands-on/Apache Spark/Pyspark/pyspark_create_emptyRDD().py
@@ -7,3 +7,17 @@ print(emptyRDD)
 #alternatively create empty RDD in PySpark using parallelize() from SparkContext 
 rdd2 = spark.sparkContext.parallelize([])
 print(rdd2)
+
+# Create an empty Dataframe with schema 
+from pyspark.sql.types import StructType, StructField, StringType
+schema = StructType([
+    StructField('firstname', StringType(), True),
+    StructField('middlename', StringType(), True),
+    StructField('lastname', StringType(), True)])
+
+
+# Create an empty Dataframe from an empty RDD 
+
+df = spark.createDataFrame(emptyRDD, schema)
+df.printSchema() 
+
