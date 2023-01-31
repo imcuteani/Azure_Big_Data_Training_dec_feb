d011478e207a7c81b9c4d57037e6d08443e58d82
Anindita Basak <anindita.basak@live.com>
2023-01-31 16:19:07 +0530

    added Spark SQL

--- Hands-on/Apache Spark/Pyspark/pyspark_df.py
+++ Hands-on/Apache Spark/Pyspark/pyspark_df.py
@@ -4,3 +4,7 @@ spark = SparkSession.builder.master("local[1]").appName('CGSparkDemo').getOrCrea
 # Create Dataframe in PySpark 
 df = spark.createDataFrame([("Scala", 20000), ("Spark", 30000), ("PHP", 25000)])
 df.show()  # display the dataframe
+#Spark SQL 
+df.createOrReplaceTempView("sample_table")
+df2 = spark.sql("SELECT _1, _2 FROM sample_table")
+df2.show()
